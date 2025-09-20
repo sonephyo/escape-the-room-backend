c7891ee41ebe03a3bd7d05044dfb0080f1894def
shusank8 <shusank8basyal@gmail.com>
2025-09-20 00:45:44 -0400

    fake data

--- utils.py
+++ utils.py
@@ -1,2 +1,12 @@
+
+data = [{
+    "id":1,"name":"Ronaldo", "description":"Best football player"
+},{
+    "id":2,"name":"Ronaldo", "description":"Best football player"
+},{
+    "id":3,"name":"Ronaldo", "description":"Best football player"
+},{
+    "id":4,"name":"Ronaldo", "description":"Best football player"
+},]
 def get_puzzle():
     pass
