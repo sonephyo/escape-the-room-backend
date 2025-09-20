b574c41ea07e0de0b171150499cbb8444f219dff
shusank8 <shusank8basyal@gmail.com>
2025-09-20 02:33:16 -0400

    feat: generate puzzle

--- utils.py
+++ utils.py
@@ -1,12 +1,59 @@
+from prompts import puzzle_prompt
+from dotenv import load_dotenv
+
+from google.genai import types
+from google import genai
+import asyncio 
+import os
+from pydantic import BaseModel
+
+load_dotenv() 
+gemini_api_key = os.getenv("GEMINI_API_KEY")
 
 data = [{
     "id":1,"name":"Ronaldo", "description":"Best football player"
-},{
-    "id":2,"name":"Ronaldo", "description":"Best football player"
-},{
-    "id":3,"name":"Ronaldo", "description":"Best football player"
-},{
-    "id":4,"name":"Ronaldo", "description":"Best football player"
-},]
-def get_puzzle():
-    pass
+}
+,{
+    "id":2,"name":"Mr Beast", "description":"Youtuber"
+}
+]
+
+class Recipe(BaseModel):
+    Puzzle: str
+
+
+
+
+async def generate_for_item(client, puzzle_prompt):
+    
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=puzzle_prompt,
+        config={
+        "response_mime_type": "application/json",
+        "response_schema": Recipe,
+    },
+        
+    )
+    return response.parsed
+
+    
+
+async def get_puzzle(data):
+    client = genai.Client()
+    tasks = []
+    for x in data:
+    #     print("starting==")
+        tasks.append(generate_for_item(client, puzzle_prompt))
+    #     print('----finish------')
+    res = await asyncio.gather(*tasks)
+
+
+    for idx, x in enumerate(data):
+        x['puzzle'] = res[idx].Puzzle
+
+    return data
+
+# # To run
+# ans = asyncio.run(get_puzzle(data))
+# print(ans)
